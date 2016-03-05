95efdc10dbfdb459a1b492166241c2993a494ecd
nonylene <cookie.speaker@gmail.com>
2016-03-06 05:21:18 +0900

    APIを取得、Toastで表示

--- app/src/main/java/net/nonylene/stacker/MainActivity.java
+++ app/src/main/java/net/nonylene/stacker/MainActivity.java
@@ -5,6 +5,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +31,39 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String searchText = editText.getText().toString();
+                requestApi(searchText);
+            }
+        });
+    }
+
+    private void requestApi(String text) {
+        String encodedText = null;
+        try {
+            encodedText = URLEncoder.encode(text, "utf-8");
+        } catch (UnsupportedEncodingException ignore) {}
+
+        String url = "https://api.stackexchange.com/2.2/search?order=desc&sort=activity&intitle=" +
+                encodedText + "&site=stackoverflow&filter=!6JEajsykLFu7H";
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        new OkHttpClient().newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                final String body = response.body().string();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, body, Toast.LENGTH_LONG).show();
+                    }
+                });
             }
         });
     }
